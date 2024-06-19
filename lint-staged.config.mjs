71ae3fa8655eaef9173d7bb4e53daeb89048a8f3
viet34tqc <viet34tqc@gmail.com>
2024-06-19 23:25:27 +0700

    Fix lint-staged for NextJS project

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,4 +1,11 @@
+import path from 'path'
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`
+
 const config = {
-  '*.{js,jsx,ts,tsx,css}': ['pnpm lint', 'pnpm format', 'git add .'],
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand, 'pnpm format', 'git add .'],
 }
 export default config
