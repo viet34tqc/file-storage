678967908c929c05825101cba793661db5a2f600
viet34tqc <viet34tqc@gmail.com>
2024-07-04 23:38:23 +0700

    Update favicon and title metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'File Storage',
+}
 
 // Copyright: https://tailwindui.com/components/marketing/sections/heroes
 
